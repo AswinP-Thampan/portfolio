8c7c0ceed32be925db0960725042f0f186cf1c49
AswinP-Thampan <aswinp.thampan@gmail.com>
2025-05-18 06:03:41 +0530

    layout and cleanup

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,15 +1,68 @@
 "use client"
 
 import { MotionDock } from "../page";
+import { motion } from "framer-motion";
+import Image from 'next/image'
 
 
 export default function Profile() {
 
+    const reveal = {
+        hidden: { opacity: 0, y: 30 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: { duration: 0.6, ease: "easeOut" },
+        },
+    };
+
     return (
-        <div className="h-screen w-screen flex flex-col font-[family-name:var(--font-geist-sans)]">
+        <div className="h-screen w-screen flex flex-col font-[family-name:var(--font-geist-sans)] overflow-x-hidden">
             <MotionDock isTop={false} />
-            <div className="flex flex-col items-center">
-                {/* <span className="text-black">Hi There</span> */}
+            <div className="flex flex-col items-center mt-32 gap-10 text-3xl">
+                <motion.div
+                    className="flex flex-row gap-5 rounded-3xl max-w-[90%] w-[700px] "
+                    variants={reveal}
+                    initial="hidden"
+                    animate="visible">
+                    <div>
+                        About
+                        <div className="w-52 h-52 rounded-full bg-[#dd3333] border-4 border-white shadow-lg flex items-center justify-center relative overflow-hidden">
+                            <Image
+                                src="/pfp.png"
+                                alt="Picture of the Author"
+                                fill
+                                className="object-cover scale-120 translate-y-5 translate-x-2"
+                            />
+                        </div>
+
+                    </div>
+                    <div className="text-lg pt-12">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra sem vitae ex bibendum, sed gravida nunc placerat. Praesent quis elementum neque. Ut turpis nisl, consectetur elementum nibh a, feugiat tincidunt augue. Etiam pulvinar ac mauris sed iaculis. Praesent scelerisque urna malesuada, varius velit nec, varius lacus. Quisque aliquam est ac velit cursus, vitae suscipit diam suscipit. Ut condimentum tortor sed pellentesque commodo.
+                    </div>
+                </motion.div>
+                <motion.div
+                    className="flex flex-col gap-5 max-w-[90%] pb-10 overflow-x-hidden "
+                    variants={reveal}
+                    initial="hidden"
+                    animate="visible">
+                    Details
+                    {[1, 2, 3, 4].map((_, i) => (
+                        <motion.div
+                            key={i}
+                            className="bg-[#dddddd33] border-2 rounded-3xl p-10 w-[700px]"
+                            variants={reveal}
+                            initial="hidden"
+                            animate="visible"
+                            transition={{ delay: 0.3 + i * 0.15 }}
+                        >
+                            Details #{i}
+                            <div className="text-lg">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur viverra sem vitae ex bibendum, sed gravida nunc placerat. Praesent quis elementum neque. Ut turpis nisl, consectetur elementum nibh a, feugiat tincidunt augue. Etiam pulvinar ac mauris sed iaculis. Praesent scelerisque urna malesuada, varius velit nec, varius lacus. Quisque aliquam est ac velit cursus, vitae suscipit diam suscipit. Ut condimentum tortor sed pellentesque commodo.
+                            </div>
+                        </motion.div>
+                    ))}
+                </motion.div>
             </div>
         </div>
 
